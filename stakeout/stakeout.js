ffe5dda3b9cb1c7311f2c865a521b37fb31a532f
Iria Tam <iria@smugmug.com>
2015-05-18 14:42:06 -0700

    fix [2,4,2] test case

--- stakeout/stakeout.js
+++ stakeout/stakeout.js
@@ -5,18 +5,34 @@
     var updateOffLimitsMap = function(index) {
         if(index - 1 >= 0) {
             this.offLimitsMap[index - 1] = true;
-            // console.log('added index', index - 1);
         }
         if(index + 1 <= this.maxIndex) {
-            this.offLimitsMap[index + 1] = true;
-            // console.log('added index', index + 1);            
+            this.offLimitsMap[index + 1] = true;     
+        }
+    };
+    
+    var calculateMax = function(valueArray, originalIndexMap) {
+        var total = 0;
+        
+        for(var i = 0; i <= this.maxIndex; i ++) {
+            var originalIndex = originalIndexMap[valueArray[i]];
+            
+            if(typeof originalIndex === 'object') {
+                originalIndex = originalIndex.pop();
+            }
+
+            if(!this.offLimitsMap[originalIndex]) {     
+                total += valueArray[i];
+                updateOffLimitsMap(originalIndex);
+            }
         }
         
-        // console.log('offLimitsMap ', this.offLimitsMap);
+        return total;
     };
     
     var getOptimalResult = function(valueArray) {    
         var indexMap = {};
+        var indexMapCopy = {};
         var total = 0;
         
         this.offLimitsMap = {};
@@ -25,58 +41,41 @@
         valueArray.forEach(function(item, index) {
             if(typeof indexMap[item] === 'undefined') {
                 indexMap[item] = index;
+                indexMapCopy[item] = index;
             } else {
                 if(!(typeof indexMap[item] === 'object')) {
-                    indexMap[item] = [indexMap[item], index];                    
+                    indexMap[item] = [indexMap[item], index];
+                    indexMapCopy[item] = [indexMapCopy[item], index];   
                 } else {
                     indexMap[item].push(index);
+                    indexMapCopy[item].push(index);
                 }
             }
         });
-        
-        // console.log(indexMap);
         
         valueArray.sort(function(a, b) {
             return a < b;
         });
         
-        // console.log(valueArray);
+        var firstMax = calculateMax(valueArray, indexMap);
         
-        for(var i = 0; i <= this.maxIndex; i ++) {
-            var originalIndex = indexMap[valueArray[i]];
-
-            // console.log('indexMap: ', indexMap[valueArray[i]]);
-            
-            if(typeof originalIndex === 'object') {
-                originalIndex = originalIndex.pop();
-            }
-            
-            // console.log('index: ', i);
-            // console.log('originalIndex:', originalIndex);
-            
-            // console.log('offLimitsMap ', this.offLimitsMap);
-
-            if(!this.offLimitsMap[originalIndex]) {
-                // console.log('added: ', valueArray[i]);
-                
-                total += valueArray[i];
-                updateOffLimitsMap(originalIndex);
-                
-                // console.log('new total: ', total);
-            }
-        }
+        var secondArray = valueArray.splice(1);
+        this.offLimitsMap = {};
+        this.maxIndex = secondArray.length - 1;
         
-        return total;
+        var secondMax = calculateMax(secondArray, indexMapCopy);
+        
+        return (firstMax > secondMax) ? firstMax : secondMax;
     };
-    
-    //console.log(getOptimalResult([ 20, 10, 50, 5, 1 ]));
-    //console.log(getOptimalResult([ 20, 50, 10, 1, 5 ]));
-    //console.log(getOptimalResult([5, 5, 5, 5, 5]));
-    //console.log(getOptimalResult([1, 2, 3, 4, 5]));
-    //console.log(getOptimalResult([32, 12, 1, 34, 60]));
-    //console.log(getOptimalResult([1, 24, 15, 32, 14, 5, 6]));
-    //console.log(getOptimalResult([15, 12, 99, 5, 16, 12]));
-    //console.log(getOptimalResult([ 15, 12, 99, 5, 16, 12 ]));
-    //console.log(getOptimalResult([]));
+      
+    console.log(getOptimalResult([ 20, 10, 50, 5, 1 ]));
+    console.log(getOptimalResult([ 20, 50, 10, 1, 5 ]));
+    console.log(getOptimalResult([ 15, 12, 99, 5, 16, 12 ]));
+    console.log(getOptimalResult([ 5, 5, 5, 5, 5 ]));
+    console.log(getOptimalResult([ 1, 2, 3, 4, 5 ]));
+    console.log(getOptimalResult([ 32, 12, 1, 34, 60 ]));
+    console.log(getOptimalResult([ 1, 24, 15, 32, 14, 5, 6 ]));
+    console.log(getOptimalResult([ 2, 3, 2]));
+    console.log(getOptimalResult([ ]));
 
 }());
